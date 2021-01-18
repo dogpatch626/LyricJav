4d159d23b649cd6eb46b195455c2bb747621d509
dogpatch626 <djpepnin1299@gmail.com>
2021-01-17 20:51:16 -0500

    its working now for stage 2!

--- LyricGrab.js
+++ LyricGrab.js
@@ -5,6 +5,7 @@ const readline = require("readline");
 const url = "https://genius.com/Gorillaz-november-has-come-lyrics";
 
 function getLyric(url) {
+  var src = [];
   axios.default
     .get(url)
     .then((res) => {
@@ -12,13 +13,18 @@ function getLyric(url) {
       const $ = cheerio.load(res.data);
       //const src = $("br")[4].nextSibling.data;
       //const src = $("br");
-      var src = [];
-      for (let i = 0; i < 20; i++) {
+      const size = $("br").length;
+
+      const second = $("a").attr("href");
+      for (let i = 0; i < size - 1; i++) {
         src.push($("br")[i].nextSibling.data);
         console.log(src[i]);
       }
+      //console.log(second);
+      //return src;
     })
     .catch(() => console.log("error"));
+  //return src;
 }
 
 getLyric(url);
